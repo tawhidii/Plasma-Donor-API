74da68010a457d8aca080e0fd569b7a4a66644ee
tawhidi <tawhidi.pro@gmail.com>
2020-06-12 20:24:02 +0600

    changed plasma donor blood donation model field

--- become_donors/models.py
+++ become_donors/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class BecomePlasmaDonors(models.Model):
     name = models.CharField(max_length=100,blank=False)
     blood_group = models.CharField(max_length=20,blank=False)
-    last_date_of_blood_donation = models.DateField()
+    last_date_of_blood_donation = models.CharField(max_length=60)
     address = models.CharField(max_length=500,blank=False)
     phone_number = models.CharField(max_length=20)
     city = models.CharField(max_length=50)
